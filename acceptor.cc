8e4864a0613b369fbede7a206d2251fcd741044d
kaikaizi <liulukai@gmail.com>
2013-03-31 00:52:31 -0400

    TODO: native support for encoding (rid of OpenCV API for video writing)

--- acceptor.cc
+++ acceptor.cc
@@ -1,3 +1,19 @@
+/*  Copyright (C) 2012  Liu Lukai	(liulukai@gmail.com)
+    This file is part of VideoAnalyzer.
+
+    VideoAnalyzer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
 #include <cstdint>
 #include <iostream>
 #include <boost/thread.hpp>
@@ -183,7 +199,6 @@ public: /* VideoDecoder must outlives this */
 	   if(packet.stream_index==rtp.getStreamIndex() &&
 		   avcodec_decode_video2(rtp.getCodecContext(),pic,&check,&packet)>=0
 		   && check){
-		puts("OK");
 		if(tp)sws_scale(img_context_ctx,pic->data,pic->linesize,0,height,
 			picbgr->data,picbgr->linesize);
 		if(++indx>=std::numeric_limits<int>::max())indx=0;
